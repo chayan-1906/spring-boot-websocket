fdb1816eca84a84fbe5c81cd90d99ca94348535d
Padmanabha Das <82430454+chayan-1906@users.noreply.github.com>
2022-10-25 00:14:31 +0530

    youtube link pasted in main class

--- src/main/java/com/padmanabhasmac/spring/websocket/api/SpringBootWebsocketApplication.java
+++ src/main/java/com/padmanabhasmac/spring/websocket/api/SpringBootWebsocketApplication.java
@@ -3,6 +3,8 @@ package com.padmanabhasmac.spring.websocket.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* https://youtu.be/4Hyv4M1kFeM?list=PLVz2XdJiJQxw-jVLpBfVn2yqjvA1Ycceq */
+
 @SpringBootApplication
 public class SpringBootWebsocketApplication {
 
